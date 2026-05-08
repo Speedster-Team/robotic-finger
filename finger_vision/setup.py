7299b75ff10e6b771b0cd5e2c4a78da919a7b0a3
Mjenz <michael.jenz77@gmail.com>
2026-05-08 16:27:00 -0500

    camera streaming working

--- finger_vision/setup.py
+++ finger_vision/setup.py
@@ -1,6 +1,23 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 package_name = 'finger_vision'
+
+
+def recursive_files(prefix, path):
+    """
+    Recurse over path returning a list of tuples suitable for use with setuptools data_files.
+
+    :param prefix: prefix path to prepend to the path
+    :param path: Path to directory to recurse. Path should not have a trailing '/'
+    :return: List of tuples. First element of each tuple is destination path,
+        second element is a list of files to copy to that path
+    """
+    return [(str(Path(prefix)/subdir),
+            [str(file) for file in subdir.glob('*') if not file.is_dir()])
+            for subdir in Path(path).glob('**')]
+
 
 setup(
     name=package_name,
@@ -10,6 +27,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        *recursive_files('share/' + package_name, 'launch'),
+        *recursive_files('share/' + package_name, 'config'),
+
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
@@ -17,7 +37,7 @@ setup(
     maintainer='Mjenz',
     maintainer_email='michael.jenz77@gmail.com',
     description='Identify and publish waypoints using CV',
-    license='MIT',
+    license=' Apache 2.0',
     extras_require={
         'test': [
             'pytest',
